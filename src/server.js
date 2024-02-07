7c8701e534b657137b23578179cd55c8c2df9c71
hong3737 <qwert3748@gmail.com>
2024-02-07 17:54:19 +0900

    morgan

--- src/server.js
+++ src/server.js
@@ -1,17 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+
 const handleHome = (req, res) => {
     return res.send("I still love you.");
 };
-const handleLogin = (req, res) => {
-    return res.send("Login here.");
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome to private lounge");
 };
 
+app.use(logger);
 app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
     console.log(`Server listening on port http://localhost:${PORT}`);
