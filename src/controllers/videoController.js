ac6f498fb74d6cfee443627bc8c5ebcd69fea640
hong3737 <qwert3748@gmail.com>
2024-02-13 15:16:55 +0900

    pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,9 @@
-export const trending = (req, res) => res.send("Home Page Vidios");
+export const trending = (req, res) =>
+    res.render("home", { pageTitle: "Home", potato: "tomato" });
 export const search = (req, res) => res.send("Search video");
 export const upload = (req, res) => res.send("Upload Vidio");
-export const see = (req, res) => {
-    return res.send(`Watch Vidio #${req.params.id}`);
-};
-export const edit = (req, res) => {
-    return res.send(`Edit Vidio #${req.params.id}`);
-};
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const deleteVideo = (req, res) => {
     return res.send(`Delete Vidio #${req.params.id}`);
 };
